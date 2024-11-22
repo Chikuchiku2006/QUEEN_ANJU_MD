3661e050fee779f038181e0e5badc6d3760c5ec6
Chikuchiku2006 <janithmawanigasooriya2006@gmail.com>
2024-11-22 22:42:04 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=WdEQkbLB#OWj7B0-_NpW8IelQbMURe3IGi-ruq7KFVmN0qIQaWNo",
 };
